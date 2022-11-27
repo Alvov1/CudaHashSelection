695b9a6050585ce197c14d9da77eac306eff4c4f
Alexander <al.lvov777@gmail.com>
2022-11-28 00:00:38 +0300

    OssException.h

---
+++ CPU/OssException.h
@@ -0,0 +1,15 @@
+#ifndef HASHSELECTION_OSSEXCEPTION_H
+#define HASHSELECTION_OSSEXCEPTION_H
+
+#include <iostream>
+#include <openssl/err.h>
+
+class OssException final : public std::exception {
+    std::string message;
+public:
+    explicit OssException(std::string message)
+    : message(std::move(message) + ": " + ERR_error_string(ERR_get_error(), nullptr)) {}
+    const char *what() noexcept { return message.c_str(); }
+};
+
+#endif //HASHSELECTION_OSSEXCEPTION_H
