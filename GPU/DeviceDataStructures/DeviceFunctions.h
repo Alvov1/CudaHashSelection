027482930d31379b89e50032f1e3f008eb2c66b7
Alexander <al.lvov777@gmail.com>
2022-12-03 23:30:11 +0300

    Improvements. Crashes for some reason, maybe because of recursion.

--- GPU/DeviceDataStructures/DeviceFunctions.h
+++ GPU/DeviceDataStructures/DeviceFunctions.h
@@ -18,6 +18,7 @@ namespace DeviceFunctions {
 
     template <typename T>
     DEVICE bool hashAndCompare(const T& candidate, const DeviceWord& requiredHash) {
+        printf("Candidate: %s\n", candidate.get());
         DeviceSHA256 hash(candidate.get(), candidate.size());
         const auto& value = hash.toVector();
         return requiredHash == value;
@@ -25,6 +26,9 @@ namespace DeviceFunctions {
 
     DEVICE bool nextPermutation(const DeviceWord& candidate, const DeviceWord& requiredHash,
                                 DeviceVector<char>& buffer, const char* const* mutations, char* const placeForResult) {
+        static unsigned recursionCount = 1;
+        printf("Recursion: %u. Buffer: %s. BufferSize = %llu. Candidate: %s\n",
+               recursionCount++, buffer.get(), buffer.size(), candidate.get());
         /* Recursion out. */
         if(buffer.size() == candidate.size() && hashAndCompare(buffer, requiredHash))
             return fillSolution(buffer.get(), buffer.size(), placeForResult);
@@ -32,6 +36,7 @@ namespace DeviceFunctions {
         /* Recursion continue; */
         const unsigned position = buffer.size();
         buffer.push_back(candidate[position]);
+        printf("Pushed back. Buffer: %s, bufferSize = %llu, candidate: %s\n", buffer.get(), buffer.size(), candidate.get());
         if(nextPermutation(candidate, requiredHash, buffer, mutations, placeForResult)) return true;
         buffer.pop_back();
 
@@ -57,20 +62,20 @@ namespace DeviceFunctions {
                    const char* const* mutations, unsigned wordsPerThread,
                    const char* requiredHash, char* placeForResult) {
         const auto threadNumber = blockIdx.x * blockDim.x + threadIdx.x;
+        if(threadNumber > 0) return;
 
         auto passwordPosition = threadNumber * wordsPerThread;
         if(passwordPosition >= passSize) return;
 
         DeviceWord current(passwords[passwordPosition]);
-        if(hashAndCompare(current, requiredHash)) {
-            fillSolution(current.get(), current.size(), placeForResult);
+//        if(hashAndCompare(current, requiredHash)) {
+//            fillSolution(current.get(), current.size(), placeForResult);
+//            return;
+//        }
+
+        DeviceVector<char> buffer(current.size());
+        if(nextPermutation(current, requiredHash, buffer, mutations, placeForResult))
             return;
-        }
-
-//        DeviceVector<char> buffer(current.length(), '\0');
-//        if(nextPermutation(current, requiredHash, buffer, mutations, resultBuffer))
-//            return; /* Solution found in our thread. */
-
     }
 }
 
