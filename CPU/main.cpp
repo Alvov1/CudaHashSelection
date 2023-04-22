39f654ca1b1d3f3f1539beb29aed951394b43f7d
alexanderlvov <al.lvov777@gmail.com>
2023-04-22 18:55:47 +0300

    Very small improvements.

--- CPU/main.cpp
+++ CPU/main.cpp
@@ -3,27 +3,6 @@
 #include "PasswordDictionary.h"
 
 int main() {
-    MutationDictionary replacements;
-    replacements.show();
 
-    PasswordDictionary passwords;
-    passwords.calculateQuantities(replacements);
-
-    const auto& plainPassword = passwords.getRandom();
-    Console::timer << "Using word: " << plainPassword << Console::endl;
-
-    const auto mutatedPassword = replacements.mutate(plainPassword);
-    Console::timer << "Using after rearrangement: " << mutatedPassword << Console::endl;
-
-    const HostSHA256 hash(mutatedPassword.c_str(), mutatedPassword.size());
-    Console::timer << "Searching for mutatedPassword with hash '" << hash.to_string() << "'." << Console::endl;
-
-    std::function comparator =
-        [](const std::string& buffer, const std::string& requiredHash) {
-            HostSHA256 currentHash(buffer.c_str(), buffer.size());
-            return currentHash.to_string() == requiredHash;
-        };
-
-    passwords.find(replacements, hash.to_string(), comparator);
     return 0;
 }
--- MutationsTest/main.cpp
+++ MutationsTest/main.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
 #include <array>
 
-const std::array<std::string_view, 26>& get() {
-    static constexpr std::array<std::string_view, 26> words = {
-            /* A */ "4@^",  /* B */ "86",     /* C */ "[<(",  /* D */ "",       /* E */ "3&",
-            /* F */ "v",    /* G */ "6&9",    /* H */ "#",    /* I */ "1|/\\!", /* J */ "]}",
-            /* K */ "(<x",  /* L */ "!127|",  /* M */ "",     /* N */ "^",      /* O */ "0",
-            /* P */ "9?",   /* Q */ "20&9",   /* R */ "972",  /* S */ "3$z2",   /* T */ "7+",
-            /* U */ "v",    /* V */ "u",      /* W */ "v",    /* X */ "%",      /* Y */ "j", /* Z */ "27s"
-    };
+#include "Word.h"
 
-    return words;
-}
 
-template <std::size_t size = 20>
-void process(const std::string_view& view, const std::array<char, size>& buffer) {
-
-}
 
 int main() {
 
